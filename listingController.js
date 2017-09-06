44e2b39544c6328e4a3133560cc567990d684a29
Katie Harrington <katherinegharrin@ufl.edu>
2017-09-06 13:52:36 -0400

    Completed Assignment 2
    Done~

--- listingController.js
+++ listingController.js
@@ -4,11 +4,38 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
 
     /* 
-      Implement these functions in the controller to make your application function 
+      :) Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function () {
+        $scope.listings.push({
+            'code': $scope.code,
+            'name': $scope.name,
+            'coordinates.latitude': $scope.latitude,
+            'coordinates.longitude': $scope.longitude,
+            'address': $scope.address
+        });
+        $scope.code = "";
+        $scope.name = "";
+        $scope.latitude = "";
+        $scope.longitude = "";
+        $scope.address = "";
+    };
+
+    $scope.deleteListing = function (index) {
+        var place = -1;
+        var arr = eval($scope.listings);
+        for (var i = 0; i < arr.length ; i++) {
+            if (arr[i].code === index) {
+                place = i;
+                break;
+            }
+        }
+        $scope.listings.splice(place, 1);
+    };
+
+    $scope.showDetails = function (index) {
+        $scope.detailedInfo=index;
+    };
   }
 ]);
